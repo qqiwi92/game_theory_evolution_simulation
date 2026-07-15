954f88fbc9d21b50c13c8d83336e6bb403c254ef
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-07-15 23:36:02 +0300

    add rand prng gen in main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,8 @@
 const std = @import("std");
+const player = @import("player.zig");
+pub fn main() !void {
+    var prng = std.Random.DefaultPrng.init(1337);
+    const rand = prng.random();
+    const p = player.Player.initRandom(0, rand);
+    _ = p.procreate(2, rand);
+}
