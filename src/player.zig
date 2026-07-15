05e23f76d6276a9768d26fd6bc732d1b7a184878
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-07-15 21:20:45 +0300

    init with random

--- src/player.zig
+++ src/player.zig
@@ -1,17 +1,40 @@
 const std = @import("std");
 
-const Player = struct {
-    var c_trust: f32 = 0.5;
-    var c_carma: f32 = 0.5;
-    var c_defection: f32 = 0.5;
+pub const Coefficients = struct {
+    trust: f32 = 0.5,
+    carma: f32 = 0.5,
+    defection: f32 = 0.5,
+    
+    pub fn initRandom() Coefficients {
+        const rand = std.crypto.random;
+        return Coefficients{
+            .trust = rand.float(f32),
+            .carma = rand.float(f32),
+            .defection = rand.float(f32),
+        };
+    }
+};
+
+pub const Player = struct {
+    id: u32,
+    coefs: Coefficients,
 
     const Self = @This();
 
-    pub fn init() Self {
+    pub fn init(id: u32) Self {
         return .{
-            .c_trust = 0.5,
-            .c_carma = 0.5,
-            .c_defection = 0.5,
+            .id = id,
+            .coefs = .{},
         };
+    }
+
+    pub fn initWithCoefs(id: u32, coefs: Coefficients) Self {
+        return .{
+            .id = id,
+            .coefs = coefs,
+        };
+    }
+    pub fn initRandom(id: u32) Self{
+        return initWithCoefs(id, Coefficients.initRandom());
     }
 };
