1e5387523a1db28b495553d27cd6a58bda7ee9fe
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-07-16 01:25:05 +0300

    added weights

---
+++ src/weights.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub const Choice = enum(u1) {
+    cooperate = 0,
+    defect = 1,
+};
+
+pub const Payoff = struct {
+    p1: i32,
+    p2: i32,
+};
+
+pub const payoff_matrix = [2][2]Payoff{
+    .{
+        .{ .p1 = 3, .p2 = 3 },
+        .{ .p1 = 0, .p2 = 5 },
+    },
+    .{
+        .{ .p1 = 5, .p2 = 0 },
+        .{ .p1 = 1, .p2 = 1 },
+    },
+};
+
+pub fn getPayoff(p1_choice: Choice, p2_choice: Choice) Payoff {
+    const r1 = @intFromEnum(p1_choice);
+    const r2 = @intFromEnum(p2_choice);
+    return payoff_matrix[r1][r2];
+}
